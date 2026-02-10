debbd6ddf5ccf9e53e325b8ec6b573a1dad83494
rabeea-ahmed <ra4667@nyu.edu>
2026-02-11 02:59:55 +0400

    Project Test

--- project.js
+++ project.js
@@ -10,9 +10,9 @@ let isLiked = false; // start unliked
 
 heart.addEventListener("click", function() {
   if (isLiked) {
-    heart.src = "/assets/empty_heart.png";
+    heart.src = "empty_heart.png";
   } else {
-    heart.src = "/assets/heart.png";
+    heart.src = "heart.png";
   }
   isLiked = !isLiked;
 });
