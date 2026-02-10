17d14289d1853afad6e7e8d4cf840428aee048ed
rabeea-ahmed <ra4667@nyu.edu>
2026-02-11 02:37:07 +0400

    Project

---
+++ project.js
@@ -0,0 +1,18 @@
+const img = document.getElementById("toggleImage");
+const section = document.getElementById("comment");
+
+img.addEventListener("click", () => {
+  section.classList.toggle("comment");
+});
+
+const heart = document.getElementById("heart");
+let isLiked = false; // start unliked
+
+heart.addEventListener("click", function() {
+  if (isLiked) {
+    heart.src = "/assets/empty_heart.png";
+  } else {
+    heart.src = "/assets/heart.png";
+  }
+  isLiked = !isLiked;
+});
